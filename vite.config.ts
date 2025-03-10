618c7ab71b03d165d73a107b9bd60062d3aa390a
satishsurani <satishsurani@gmail.com>
2025-03-10 21:54:09 +0530

    minimal change

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensure relative paths
+  base: './', // Ensures relative paths
   build: {
     outDir: 'dist',
     target: 'esnext',
